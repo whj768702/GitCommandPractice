38dadd63815ade6801767c7b2cd309a0858706f5
whj768702 <whj768702@gmail.com>
2022-03-22 16:35:30 +0800

    feat(add): 1.ts add two functions, add and deleteIndex.

--- 1.ts
+++ 1.ts
@@ -1,7 +1,15 @@
-function test(value: string): string{
+function test(value: string): string {
     if (value === 'abc') {
         return 'abc';
     } else {
         return 'nothing';
     }
+}
+
+function add() {
+    return void 0;
+}
+
+function deleteIndex() {
+    return void 0;
 }
